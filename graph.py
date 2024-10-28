c8b4c0247e1004c8667401780a908f8262dfcf02
jonleewh <94356443+jonleewh@users.noreply.github.com>
2024-10-28 19:10:06 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -4,32 +4,40 @@ import matplotlib.pyplot as plt
 G = nx.Graph()
 
 class Attraction:
-    def init_(self, name, node_type, crowd_level):
+    def __init__(self, name, node_type, zone, crowd_level):
         self.name = name
         self.node_type = node_type
+        self.zone = zone
         self.crowd_level = crowd_level
     
-    # Method to update crowd Level
+    # Method to update crowd level
     def update_crowd_level(self, new_crowd):
         self.crowd_level = new_crowd
-        print(f"(self.name) now has a crowd level of (self.crowd_level}")
+        print(f"{self.name} now has a crowd level of {self.crowd_level}")
     
     # Method to get details of the attraction
     def get_info(self):
-        return
-        # return f"{self.name) is a {self.node_type) with a current crowd level of {self.crowd)
+        print(f"{self.name} is a {self.node_type} in {self.zone} with a current crowd level of {self.crowd_level}")
 
 # add nodes, ensure properties are in the format (key = value)
-G.add_node('Roller Coaster', type = 'ride', speed = 100, popularity = 90, wait_time = 20)
-G.add_node('Ferris Wheel', type = 'ride', speed = 10, popularity = 70, wait_time = 15)
-G.add_node("Burger Stand", type = 'F&B', menu_variety = 5, capacity = 50, crowd_size = 30)
-G.add_node('ToiletA', type = 'toilet', cleanliness = 80, proximity = 10, usage = 50)
+G.add_node('Transformers', type = 'ride', zone = 'Sci-Fi City', speed = 60, popularity = 95)
+G.add_node('Battlestar Galactica', type = 'ride', zone = 'Sci-Fi City', speed = 80, popularity = 88)
+G.add_node('Revenge of the Mummy', type = 'ride', zone = 'Ancient Egypt', speed = 70, popularity = 90)
+G.add_node('Jurassic Park Rapids', type = 'ride', zone = 'The Lost World', speed = 50, popularity = 85)
+G.add_node('Shrek 4D', type = 'ride', zone = 'Far Far Away', speed = 10, popularity = 75)
+G.add_node('Far Far Away Castle', type = 'attraction', zone = 'Far Far Away', crowd_level = 70)
+G.add_node('WaterWorld', type = 'show', zone = 'Hollywood', duration = 30, capacity=100, crowd_level=60)
+G.add_node('Hawker’s Market', type = 'F&B', zone = 'New York', menu_variety=10, capacity=150, crowd_size=80)
+G.add_node('Toilets', type = 'toilet', zone = 'Main Street', cleanliness=90, proximity=5, usage=30)
 
 # add edges, each number represents the node
-G.add_edge('Roller Coaster', 'Ferris Wheel', distance = 200)
-G.add_edge('Roller Coaster', 'Burger Stand', distance = 100)
-G.add_edge('Ferris Wheel', 'ToiletA', distance = 50)
-G.add_edge('ToiletA', 'Burger Stand', distance = 70)
+G.add_edge('Transformers', 'Revenge of the Mummy', distance=150)
+G.add_edge('Revenge of the Mummy', 'Jurassic Park Rapids', distance=200)
+G.add_edge('Jurassic Park Rapids', 'Battlestar Galactica', distance=250)
+G.add_edge('Battlestar Galactica', 'Shrek 4D', distance=100)
+G.add_edge('Shrek 4D', 'WaterWorld', distance=200)
+G.add_edge('WaterWorld', 'Far Far Away Castle', distance=150)
+G.add_edge('Hawker’s Market', 'Toilets', distance=30)
 
 # visualisation of graph
 nx.draw(G, with_labels=True)
