8c90107245f67cbd238244ae498002bae050cec2
Jin1402 <157813783+Jin1402@users.noreply.github.com>
2024-10-31 15:38:33 +0800

    update attendance.py

--- attendance.py
+++ attendance.py
@@ -1,7 +1,19 @@
-import csv
+import pandas as pd
 
-# Open and read the CSV file
-with open('data.csv', mode='r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+# Load the CSV file into a DataFrame
+
+
+jan = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_jan_18521_2024_10_31_15_23_50.csv')
+feb = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_feb_18521_2024_10_31_15_23_46.csv')
+mar = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_mar_18521_2024_10_31_15_23_41.csv')
+apr = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_apr_18521_2024_10_31_15_23_37.csv')
+may = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_may_18521_2024_10_31_15_23_34.csv')
+jun = pd.read_csv()
+jul = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_jul_18521_2024_10_31_15_23_26.csv')
+aug = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_aug_18521_2024_10_31_15_23_21.csv')
+sep = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_sep_18521_2024_10_31_15_23_10.csv')
+oct = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_oct_18521_2024_10_31_15_23_05.csv')
+nov = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_nov_18521_2024_10_31_15_22_53.csv')
+dec = pd.read_csv('USS daily waiting time/download_universal-studios-singapore_dec_18521_2024_10_31_14_44_27.csv')
+# Display the DataFrame
+print(apr)
