315d62de8a6851575d59c50067e39c57fbd4c9d3
jonleewh <94356443+jonleewh@users.noreply.github.com>
2024-11-10 03:47:10 +0800

    Update combine_weather_data.py

--- Group B/src/combine_weather_data.py
+++ Group B/src/combine_weather_data.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 # Daily Weather Data
 daily_weather_data = pd.read_csv("Group B/data/daily_weather_data.csv")
-temperature_data = pd.DataFrame(daily_weather_data[["Year", "Month", "Day", "Mean Temperature (°C)"]])
+temperature_data = daily_weather_data[["Year", "Month", "Day", "Mean Temperature (°C)"]]
+print(temperature_data)
 # monthly_avg = temperature_data.groupby(['Month'])['Mean Temperature (°C)'].mean()
 
 #print(monthly_avg)
