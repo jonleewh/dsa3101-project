5268cf80ccc9ebe94cf822a855af758818a1c808
jonleewh <94356443+jonleewh@users.noreply.github.com>
2024-11-10 03:03:52 +0800

    renamed weather data files

--- Group B/src/combine_weather_data.py
+++ Group B/src/combine_weather_data.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
-weather_data = pd.read_csv("Group B/data/weather_data.csv")
+# Daily Weather Data
+daily_weather_data = pd.read_csv("Group B/data/daily_weather_data.csv")
 
-temperature_data = pd.DataFrame(weather_data[["Year", "Month", "Day", "Mean Temperature (°C)"]])
+temperature_data = pd.DataFrame(daily_weather_data[["Year", "Month", "Day", "Mean Temperature (°C)"]])
 monthly_avg = temperature_data.groupby(['Month'])['Mean Temperature (°C)'].mean()
 
-print(monthly_avg)
+#print(monthly_avg)
+
+# Hourly Weather Data
+#hourly_weather_data = pd.read_csv("Group B/data/hourly_weather_data.csv")
+
